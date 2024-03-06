2af180525c05cd5f7cde8d291fe47684a325b89b
Ruiz de Azua Ayastuy, Pablo <cooperacion-110884@tecnalia.com>
2024-03-06 09:16:27 +0100

    Mensajes de error y de funcionamiento cambiados.

--- src/core/db_tests/test_execution.py
+++ src/core/db_tests/test_execution.py
@@ -22,7 +22,7 @@ class TestSqlAlchemyORM(unittest.TestCase):
 
         for cls in classes:
 
-            # Crear tabla
+            # Crear tablas
             if not inspect(self.engine).has_table(cls.__tablename__):
                 Base.metadata.tables[cls.__tablename__].create(bind=self.engine)
             self.assertTrue(inspect(self.engine).has_table(cls.__tablename__), f"Tabla {cls.__name__} no creada")
@@ -48,7 +48,7 @@ class TestSqlAlchemyORM(unittest.TestCase):
             queried_instance = session.query(cls).first()
             self.assertIsNone(queried_instance, f"No se han podido eliminar los datos de {cls.__name__}")
 
-            # Eliminar tabla
+            # Eliminar tablas
             Base.metadata.tables[cls.__tablename__].drop(bind=self.engine)
             self.assertFalse(inspect(self.engine).has_table(cls.__tablename__), f"Tabla {cls.__name__} no eliminada")
 
