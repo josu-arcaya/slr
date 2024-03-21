017bdafcfb90572390263ed1ea65bb3e0bee49f2
Garai Martínez de Santos Gómez <cooperacion-110885@tecnalia.com>
2024-03-21 11:31:09 +0100

    Error de merge de tests solucionado

--- src/core/abstract_database.py
+++ src/core/abstract_database.py
@@ -14,20 +14,3 @@ class AbstractDatabase(ABC):
     @abstractmethod
     def engine(self):
         pass
-
-from abc import ABC, abstractmethod
-
-
-class AbstractDatabase(ABC):
-    @abstractmethod
-    def get_session(self):
-        pass
-
-    @abstractmethod
-    def create_database(self):
-        pass
-
-    @property
-    @abstractmethod
-    def engine(self):
-        pass
